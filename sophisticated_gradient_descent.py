e204d3d7bb4f4980a8629c2f69ce87b49e5ca897
aria mostajeran <ariamostajeran99@gmail.com>
2021-11-19 23:57:17 +0330

    fixing overfitting

--- sophisticated_gradient_descent.py
+++ sophisticated_gradient_descent.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from ANN_Project_Assets.new_loading_dataset import get_trainset, get_testset
+from ANN_Project_Assets.Loading_Datasets import get_trainset, get_testset
 
 
 def softmax(vector):
@@ -20,28 +20,23 @@ def sig_pr(x):
 
 def backpropagation(W_1, W_2, W_3, b_1, b_2, b_3, A1, A2, out, Y, X):
     dout = 2 * (out - Y)
-    # print(A2.shape)
+
     dout_dw3 = sig_pr(W_3 @ A2 + b_3) @ A2.transpose()
-    # print(dout_dw3.shape)
+
     d_w3 = np.array(dout * dout_dw3)
-    # print(d_w3.shape)
+
     d_b3 = dout * sig_pr(W_3 @ A2 + b_3)
 
-    # print(W_3.transpose().shape)
     dout_da2 = W_3.transpose() @ sig_pr(W_3 @ A2 + b_3)
     da2_dw2 = sig_pr(W_2 @ A1 + b_2) @ A1.transpose()
     da2 = W_3.transpose() @ (dout * sig_pr(W_3 @ A2 + b_3))
-    # print(dout_da2.shape)
-    # print(da2_dw2.shape)
 
     d_w2 = da2 * da2_dw2
     d_b2 = da2 * sig_pr(W_2 @ A1 + b_2)
 
     da2_da1 = W_2.transpose() @ sig_pr(W_2 @ A1 + b_2)
     da1_dw1 = sig_pr(W_1 @ X + b_1) @ X.transpose()
-    # print(da2.shape)
-    # print(A2.shape)
-    # print(W_2.shape)
+
     da1 = W_2.transpose() @ (da2 * sig_pr(W_2 @ A1 + b_2))
     d_w1 = da1 * da1_dw1
     d_b1 = da1 * sig_pr(W_1 @ X + b_1)
@@ -49,14 +44,12 @@ def backpropagation(W_1, W_2, W_3, b_1, b_2, b_3, A1, A2, out, Y, X):
     return d_w1, d_w2, d_w3, d_b1, d_b2, d_b3
 
 
-# pics_count = 491# 100 * 100
-
+mean = 0
 test_set = get_testset()
 train_set = get_trainset()
 
 feature_count = len(train_set[0][0])
 
-# print(feature_count)
 first_layer_neurons = 100
 second_layer_neurons = 50
 output_neurons = len(train_set[0][1])
@@ -70,22 +63,19 @@ b_2 = np.zeros((second_layer_neurons, 1))
 W_3 = np.random.randn(output_neurons * second_layer_neurons).reshape(output_neurons, second_layer_neurons)
 b_3 = np.zeros((output_neurons, 1))
 
-# print(len(test_set[0][0]))
 X = [i[0] for i in train_set]
 Y = [i[1] for i in train_set]
 
 Xt = [i[0] for i in test_set]
 Yt = [i[1] for i in test_set]
 
-# feature_count = len(X[0])
-
-# print(len(X))
-
-batch_size = 15
-epochs = 30
+batch_size = 8
+epochs = 15
 learning_rate = 0.05
 total_costs = []
-optima = 0.88
+optima = 0.9
+test_accs = []
+train_accs = []
 
 for epoch in range(epochs):
     batches = []
@@ -114,7 +104,6 @@ for epoch in range(epochs):
             A1 = sigmoid(W_1 @ x + b_1)
             A2 = sigmoid(W_2 @ A1 + b_2)
             out = sigmoid(W_3 @ A2 + b_3)
-            # print(W_3.shape)
 
             gw1, gw2, gw3, gb1, gb2, gb3 = backpropagation(W_1, W_2, W_3, b_1, b_2, b_3, A1, A2, out
                                                            , y, x)
@@ -139,10 +128,6 @@ for epoch in range(epochs):
         b_2 = b_2 - (learning_rate * Vdb2)
         Vdb1 = Vdb1 * optima + (1 - optima) * (grad_b1 / batch_size)
         b_1 = b_1 - (learning_rate * Vdb1)
-
-        # b_3 = b_3 - (learning_rate * (grad_b3 / batch_size))
-        # b_2 = b_2 - (learning_rate * (grad_b2 / batch_size))
-        # b_1 = b_1 - (learning_rate * (grad_b1 / batch_size))
 
     cost = 0
     for train_data in train_set:
@@ -172,6 +157,7 @@ for epoch in range(epochs):
             corrects += 1
     accuracy = corrects / len(X)
     print("TRAIN Accuracy = ", accuracy)
+    train_accs.append(accuracy)
     corrects = 0
     for i, x in enumerate(Xt):
         # print(x.shape)
@@ -186,27 +172,12 @@ for epoch in range(epochs):
         if predicted_number == real_number:
             corrects += 1
     accuracy = corrects / len(Xt)
-    print("TEST Accuracy = ", accuracy)
+    mean += accuracy
+    # print("TEST Accuracy = ", accuracy)
+    test_accs.append(accuracy)
 
 epoch_size = [x for x in range(epochs)]
-plt.plot(epoch_size, total_costs)
-plt.savefig('20_epoch_Vectorization.png')
+plt.plot(epoch_size, train_accs)
+plt.plot(epoch_size, test_accs)
+# plt.savefig(f'{i}.png')
 plt.show()
-
-corrects = 0
-
-for i, x in enumerate(Xt):
-    # print(x.shape)
-    A1 = sigmoid(W_1 @ x + b_1)
-    A2 = sigmoid(W_2 @ A1 + b_2)
-    out = softmax(W_3 @ A2 + b_3)
-    # print("i")
-    # print(f"TEST : {Yt[i]}, PREDICT : {out}")
-    predicted_number = list(out).index(max(out))
-    real_number = list(Yt[i]).index(1)
-
-    if predicted_number == real_number:
-        corrects += 1
-accuracy = corrects / len(Xt)
-print("TEST Accuracy = ", accuracy)
-corrects = 0
